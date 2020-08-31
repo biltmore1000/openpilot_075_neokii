2e5305fc925ce09b15a02a307940a361d2628464
azure93 <azure93@gmail.com>
2020-08-31 16:14:19 +0900

    Update alerts.py
    방향등 -> 방향지시등

--- selfdrive/controls/lib/alerts.py
+++ selfdrive/controls/lib/alerts.py
@@ -60,7 +60,7 @@ class Alert():
 ALERTS = [
   Alert(
       "turningIndicatorOn",
-      "방향등 동작중엔 핸들조향 꺼짐",
+      "방향지시등 동작중엔 핸들조향 꺼짐",
       "",
       AlertStatus.userPrompt, AlertSize.small,
       Priority.HIGH, VisualAlert.none, AudibleAlert.none, 0., 0., .1),
